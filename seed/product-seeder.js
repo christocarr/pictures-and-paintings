9fcf829ffeef3fff36e24db5bc336feca396ac07
Chris Carr <christocarr@outlook.com>
2019-05-07 20:11:46 +0100

    Add product seed

---
+++ seed/product-seeder.js
@@ -0,0 +1,10 @@
+const Product = require('../models/product');
+
+const product = new Product({
+  imagePath: '',
+  title: 'Curvilinear Tree-Sold',
+  medium: 'Oils on canvas lined board',
+  size: '20 x 16 inches (508 x 406mm)',
+  price: 100
+});
+
