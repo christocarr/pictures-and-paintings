2ed904a1e2c7f5c78401b209012d603f4b12315f
Chris Carr <christocarr@outlook.com>
2019-05-08 19:41:52 +0100

    Fix mongodb document find to include render as callback

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+const Product = require('../models/product');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('gallery/index', { title: 'Express' });
+  Product.find(function(err, docs) {
+    res.render('gallery/index', { products: docs });
+
+  });
+  
+  
 });
 
 module.exports = router;
