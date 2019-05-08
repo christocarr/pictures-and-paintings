d1e246584ea861d08fab94e0f9238a6338aa7aed
Chris Carr <christocarr@outlook.com>
2019-05-08 19:42:42 +0100

    Fix broken image paths

--- seed/product-seeder.js
+++ seed/product-seeder.js
@@ -5,21 +5,21 @@ mongoose.connect('mongodb://localhost:27017/pictures-and-paintings', { useNewUrl
 
 const products = [
   new Product({
-    imagePath: '../public/images/13.Red_Evening.jpg',
+    imagePath: '/images/sold.jpg',
     title: 'Curvilinear Tree-Sold',
     medium: 'Oils on canvas lined board',
     size: '20 x 16 inches (508 x 406mm)',
     price: 100
   }),
   new Product({
-    imagePath: '../public/images/13.Red_Evening.jpg',
+    imagePath: '/images/Avenue1.jpg',
     title: 'Avenue1',
     medium: 'Oils on deep stretched canvas',
     size: '16 x 12 inches (405 x 305mm)',
     price: 75
   }),
   new Product({
-    imagePath: '../public/images/13.Red_Evening.jpg',
+    imagePath: '/images/Avenue2.jpg',
     title: 'Avenue2',
     medium: 'Oils on canvas lined board',
     size: '20 x 16 inches (508 x 406mm)',
