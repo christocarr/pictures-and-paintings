55f2a410af28799338209de4b0b610e6fe4bb696
christocarr <christocarr@outlook.com>
2019-05-17 09:03:32 +0100

    Add comment

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,15 @@ var express = require('express');
 var router = express.Router();
 const Product = require('../models/product');
 
-/* GET home page. */
+// GET home page
 router.get('/', function(req, res, next) {
   Product.find(function(err, docs) {
     res.render('gallery/index', { products: docs });
   });
 });
 
+
+// GET showpainting page and render clicked painting 
 router.get('/showpainting/:id', async (req, res, next) => {
   const painting = await Product.findById(req.params.id);
   res.render('showpainting', { painting: painting });
