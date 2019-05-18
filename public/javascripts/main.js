37f88dded37383c0a549d489fc96241d1418f696
Chris Carr <christocarr@outlook.com>
2019-05-18 09:26:57 +0100

    Add fade-in effect on page load

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,6 +1,30 @@
+
 document.addEventListener('DOMContentLoaded', ready);
                          
 function ready() {
+  //fade-in effect
+  if (!window.AnimationEvent) { return; }
+
+  const anchors = document.getElementsByTagName('a');
+
+  for (let idx = 0; idx < anchors.length; idx += 1) {
+    if (anchors[idx].hostname !== window.location.hostname) {
+      continue;
+    }
+
+    anchors[idx].addEventListener('click', function(event) {
+      const fader = document.getElementById('fader'),
+      anchor = event.currentTarget;
+      const listener = function() {
+          window.location = anchor.href;
+          fader.removeEventListener('animationend', listener);
+      };
+      fader.addEventListener('animationend', listener);
+      event.preventDefault();
+      fader.classList.add('fade-in');
+    });
+
+  }
 
   //toggle mobile nav
   const menuOpen = document.querySelector('.menu-open');
