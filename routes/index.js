6288414ffc79618f5c25e6189c7c4e02445361c8
Chris Carr <christocarr@outlook.com>
2019-05-29 19:23:26 +0100

    Add checkout post route

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,9 @@ const Cart = require('../models/cart');
 
 // GET home page
 router.get('/', function(req, res, next) {
+  let successMsg = req.flash('success')[0];
   Product.find(function(err, docs) {
-    res.render('gallery/index', { products: docs });
+    res.render('gallery/index', { products: docs, successMsg: successMsg });
   });
 });
 
@@ -45,8 +46,44 @@ router.get('/checkout', (req, res) => {
   if (!req.session.cart) {
     return res.redirect('cart');
   }
+  let errMsg = req.flash('error')[0];
   let cart = new Cart(req.session.cart);
-  res.render('checkout', { total: cart.totalPrice });
+  res.render('checkout', { total: cart.totalPrice, errMsg: errMsg });
+});
+
+router.post('/checkout', (req, res, next) => {
+  if (!req.session.cart) {
+    return res.redirect('cart');
+  }
+  
+  let cart = new Cart(req.session.cart);
+
+  // Set your secret key: remember to change this to your live secret key in production
+  // See your keys here: https://dashboard.stripe.com/account/apikeys
+  const stripe = require('stripe')('sk_test_ho39Cy3bzev0YTwr1LMEVin1005Lk87lHP');
+
+  // Token is created using Checkout or Elements!
+  // Get the payment token ID submitted by the form:
+  const token = req.body.stripeToken; // Using Express
+
+  (async () => {
+    try {
+      const charge = await stripe.charges.create({
+      amount: cart.totalPrice * 100,
+      currency: 'gbp',
+      description: 'Example charge',
+      source: token.id,
+      metadata: {order_id: 6735},
+      });  
+    } catch(err) {
+      req.flash('error', err.message);
+      return res.redirect('/checkout');
+    } 
+    req.flash('success', 'Successfully bought product');
+    req.cart = null;
+    res.redirect('/');
+  })();
+
 });
 
 module.exports = router;
